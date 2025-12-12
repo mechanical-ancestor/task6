1eb145cbfdd83970d06c6e9f8bf6574abe8f34ae
wrt2 <2664550268@example.com>
2025-12-12 08:28:45 +0800

    submit

---
+++ code/6.1.py
@@ -0,0 +1,10 @@
+
+from ultralytics import YOLO
+
+model = YOLO(r"/home/wrt/task_6/runs/detect/train1/weights/best.pt")
+model.predict(
+     source=r"/home/wrt/task_6/origin.mp4",
+     save=False,
+     show=True,
+     line_width=2,
+)

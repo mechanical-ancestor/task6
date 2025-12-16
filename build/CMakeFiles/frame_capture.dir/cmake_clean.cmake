3ad02ded5ba0d4f6efa7b7f55dbcd8916947fb74
你的GitHub用户名 <GitHub>
2025-12-16 20:52:56 +0800

    考核6

---
+++ build/CMakeFiles/frame_capture.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/frame_capture.dir/frame_cap.cpp.o"
+  "CMakeFiles/frame_capture.dir/frame_cap.cpp.o.d"
+  "frame_capture"
+  "frame_capture.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/frame_capture.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/frame_capture.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for frame_capture.
+# This may be replaced when dependencies are built.

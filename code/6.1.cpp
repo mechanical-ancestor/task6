8b0b06cc85a01f90ab0183404caab301a6a629ca
Sirius1103630 <2864425390@qq.com>
2025-12-30 22:53:55 +0800

    push task6

---
+++ code/6.1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <cstdio>
+#include <string>
+using namespace std;
+using namespace cv;
+
+
+int main( ){
+    VideoCapture video("/home/wrt/origin.avi");
+    Mat a;
+    char image[10000];
+    int frame_idx=0;
+    string path="/home/wrt/video_frame";
+     while(true){
+        video >> a;
+          if (a.empty()) break;
+                
+            
+        sprintf(image,"/home/wrt/video_frame/%06d.jpg",frame_idx++);
+            imwrite(image,a);
+     }
+     return 0;
+               
+}
